6a76b6c597e1430e4931d956864d2aea9e151db9
Jamie Coe <jamiecoe66@gmail.com>
2020-01-16 09:27:36 +0200

    fix palindrome capital letter case

--- exercises/is-palindrome/solutions/solution.js
+++ exercises/is-palindrome/solutions/solution.js
@@ -1,5 +1,6 @@
 function isPalindrome(str) {
   return str.replace(/\s|[^\w]/g, '')
+            .toLowerCase()
             .split('')
             .every((letter, index, array) => {
               return letter === array[(array.length - 1) - index]
